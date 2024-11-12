d0492b913d35e9a8dc768bffa604daefa662808b
NurTasin <nmuatasin2005@gmail.com>
2024-11-12 19:19:09 +0600

    Points to the prod https server

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,4 +4,4 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-export const ORCHESTRATOR_URL = "http://209.38.56.80:8000";
+export const ORCHESTRATOR_URL = "https://acs-streams-orchestrator.grihobangla.com";
